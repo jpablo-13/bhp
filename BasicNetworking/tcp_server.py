93396b7cd84e585ec1517be79e7348585c3c1792
jpablo <jpablo@ieee.org>
2024-12-07 18:17:36 -0300

    Simple TCP server

---
+++ BasicNetworking/tcp_server.py
@@ -0,0 +1,36 @@
+import socket
+import threading
+
+IP = '0.0.0.0'
+PORT = 9998
+
+def main():
+    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    
+    #bind the server to the previously defined listening address and port
+    server.bind((IP,PORT))
+    
+    #maximum backlog of 5 connections
+    server.listen(5)
+    print(f'[*] Listening on {IP}:{PORT}')
+
+    #start the server loop
+    while True:
+        #accept connection
+        client, address = server.accept()
+        print(f'[*] Accepted connection from {address[0]}:{address[1]}')
+        
+        #create thread for handle_client function and pass it the client socket object
+        client_handler = threading.Thread(target=handle_client, args=(client,))
+        client_handler.start()
+
+def handle_client(client_socket):
+    with client_socket as sock:
+        #receive the request
+        request = sock.recv(1024)
+        print(f'[*] Received: {request.decode("utf-8")}')
+        #send response to client
+        sock.send(b'ACK')
+
+if __name__ == '__main__':
+    main()
